868f13a1ac58573a15b41e0648e4f882c1577e8d
lincoln.mgijima <lincoln.mgijima@standardbank.co.za>
2026-01-13 16:52:50 +0200

    update

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -2,7 +2,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyC_QPmomjx6YmP55SiorvkODVHf7-z8mTA",
   authDomain: "digilayn-core-app.firebaseapp.com",
   projectId: "digilayn-core-app",
-  storageBucket: "digilayn-core-app.firebasestorage.app",
+  storageBucket: "digilayn-core-app.appspot.com",
   messagingSenderId: "362012916883",
   appId: "1:362012916883:web:577fa096e6701656912166",
   measurementId: "G-5JZ99E9GFB"
@@ -10,5 +10,17 @@ const firebaseConfig = {
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
 const db = firebase.firestore();
+const auth = firebase.auth();
+
+// Enable offline persistence
+db.enablePersistence()
+  .catch((err) => {
+    if (err.code == 'failed-precondition') {
+      // Multiple tabs open, persistence can only be enabled in one tab at a time.
+      console.warn('Firebase persistence failed: multiple tabs open.');
+    } else if (err.code == 'unimplemented') {
+      // The current browser does not support all of the features required to enable persistence.
+      console.warn('Firebase persistence failed: browser does not support it.');
+    }
+  });
